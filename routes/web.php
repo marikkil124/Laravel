285efd3a776a653a0b2763cd298073807ccf5cc3
marikkil124 <marikvalens@gmail.com>
2023-04-22 21:26:33 +0300

    Category CRUD

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,21 @@ use Illuminate\Support\Facades\Route;
 //    return view('welcome');
 //});
 
+Route::get('/', \App\Http\Controllers\Main\IndexController::class);
 
-Route::get('/', [\App\Http\Controllers\main::class,'index']) ;
+Route::prefix('admin')->group(function () {
+
+    Route::get('/', \App\Http\Controllers\Admin\Main\IndexController::class);
+
+    Route::prefix('categories')->group(function () {
+
+        Route::get('/', \App\Http\Controllers\Admin\Category\IndexController::class)
+        ->name('admin.category.index');
+        Route::get('/create', \App\Http\Controllers\Admin\Category\CreateController::class)
+        ->name('admin.category.create');
+        Route::post('/', \App\Http\Controllers\Admin\Category\StoreController::class)
+        ->name('admin.category.store');
+
+    });
+
+});
